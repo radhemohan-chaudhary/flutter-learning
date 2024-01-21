fee56962ff62a469510ae98990cf534010effee8
Radhemohan <radhechy0412@gmail.com>
2024-01-21 20:00:11 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("welcome to flutter learning"),
+          ),
+        ),
+        ),
+      );
+  }
+}
